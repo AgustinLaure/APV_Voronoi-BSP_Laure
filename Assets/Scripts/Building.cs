c5e05612001fe6acf29614cdd59453198e3a57e4
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-05-14 08:52:27 -0300

    Move room logic to room prefab

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,55 +1,6 @@
-using CustomMath;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    [SerializeField] GameObject[] roomsObj;
-
-    List<Room> rooms = new List<Room>();
-
-    private void Start()
-    {
-        SetRooms();
-    }
-
-    private void SetRooms()
-    {
-        for (int i = 0; i < roomsObj.Length; i++)
-        {
-            rooms.Add(new Room(roomsObj[i]));
-        }
-    }
-
-    public void DrawPlaneFromPoint(MyPlane plane, Vec3 point, Color color)
-    {
-        Vec3 planeCenter = point;
-        Gizmos.color = color;
-
-        Vec3 planeNormal = plane.Normal;
-        Gizmos.DrawLine(planeCenter, planeCenter + plane.Normal);
-
-        //draw tip
-        Gizmos.DrawSphere(planeCenter + plane.Normal, 0.05f);
-
-        Gizmos.color = default;
-    }
-
-    public void DrawRoomWireframe(GameObject room)
-    {
-        MeshFilter roomMesh = room.GetComponent<MeshFilter>();
-        Gizmos.DrawWireMesh(roomMesh.sharedMesh, room.transform.position, room.transform.rotation, room.transform.localScale);
-    }
-    private void OnDrawGizmos()
-    {
-        for (int i = 0; i < rooms.Count; i++)
-        {
-            for (int j = 0; j < rooms[i].planes.Length; j++)
-            {
-                DrawPlaneFromPoint(rooms[i].planes[j], rooms[i].wallsCenter[j], Color.red);
-            }
-
-            //DrawRoomWireframe(roomsObj[i]);
-        }
-    }
+    [SerializeField] Room[] roomsObj;
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,49 +1,75 @@
 using CustomMath;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Room
+public class Room : MonoBehaviour
 {
+    //[SerializeField] GameObject[] items;
+
     public MyPlane[] planes = new MyPlane[6];
     public Vec3[] wallsCenter = new Vec3[6];
+    [SerializeField] public List<Room> neighbors = new List<Room>();
 
-    public Room(GameObject room)
+    private void Awake()
     {
-        BoxCollider roomCollider = room.GetComponent<BoxCollider>();
+        BoxCollider roomCollider = GetComponent<BoxCollider>();
 
-        Vec3 roomCenter = new Vec3(room.transform.position);
+        Vec3 roomCenter = new Vec3(transform.position);
         Vec3 roomExtents = new Vec3(roomCollider.bounds.extents);
         Vec3 currentWallCenter;
         Vec3 currentDirToCenter;
 
-        wallsCenter[0] = roomCenter + (new Vec3(-room.transform.right) * roomExtents.x);
+        wallsCenter[0] = roomCenter + (new Vec3(-transform.right) * roomExtents.x);
         currentWallCenter = wallsCenter[0];
         currentDirToCenter = roomCenter - currentWallCenter;
         planes[0] = new MyPlane(currentDirToCenter, currentWallCenter);
 
-        wallsCenter[1] = roomCenter + (new Vec3(room.transform.right) * roomExtents.x);
+        wallsCenter[1] = roomCenter + (new Vec3(transform.right) * roomExtents.x);
         currentWallCenter = wallsCenter[1];
         currentDirToCenter = roomCenter - currentWallCenter;
         planes[1] = new MyPlane(currentDirToCenter, currentWallCenter);
 
-        wallsCenter[2] = roomCenter + (new Vec3(-room.transform.up) * roomExtents.x);
+        wallsCenter[2] = roomCenter + (new Vec3(-transform.up) * roomExtents.x);
         currentWallCenter = wallsCenter[2];
         currentDirToCenter = roomCenter - currentWallCenter;
         planes[2] = new MyPlane(currentDirToCenter, currentWallCenter);
 
-        wallsCenter[3] = roomCenter + (new Vec3(room.transform.up) * roomExtents.x);
+        wallsCenter[3] = roomCenter + (new Vec3(transform.up) * roomExtents.x);
         currentWallCenter = wallsCenter[3];
         currentDirToCenter = roomCenter - currentWallCenter;
         planes[3] = new MyPlane(currentDirToCenter, currentWallCenter);
 
-        wallsCenter[4] = roomCenter + (new Vec3(-room.transform.forward) * roomExtents.x);
+        wallsCenter[4] = roomCenter + (new Vec3(-transform.forward) * roomExtents.x);
         currentWallCenter = wallsCenter[4];
         currentDirToCenter = roomCenter - currentWallCenter;
         planes[4] = new MyPlane(currentDirToCenter, currentWallCenter);
 
-        wallsCenter[5] = roomCenter + (new Vec3(room.transform.forward) * roomExtents.x);
+        wallsCenter[5] = roomCenter + (new Vec3(transform.forward) * roomExtents.x);
         currentWallCenter = wallsCenter[5];
         currentDirToCenter = roomCenter - currentWallCenter;
         planes[5] = new MyPlane(currentDirToCenter, currentWallCenter);
+    }
 
+    public void DrawPlaneFromPoint(MyPlane plane, Vec3 point, Color color)
+    {
+        Vec3 planeCenter = point;
+        Gizmos.color = color;
+
+        Vec3 planeNormal = plane.Normal;
+        Gizmos.DrawLine(planeCenter, planeCenter + plane.Normal);
+
+        //draw tip
+        Gizmos.DrawSphere(planeCenter + plane.Normal, 0.05f);
+
+        Gizmos.color = default;
+    }
+
+    public void OnDrawGizmos()
+    {
+        for (int i = 0; i < planes.Length; i++)
+        {
+            DrawPlaneFromPoint(planes[i], wallsCenter[i], Color.red);
+        }
+        //DrawRoomWireframe(roomsObj[i]);
     }
 }
